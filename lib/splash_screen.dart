749f82a6c08cb1359bf611327ea66f2020b1c41a
needf <needforspeedun3@gmail.com>
2023-12-20 01:14:26 +0200

    last 2023/12/20 commit remove some lines in splash screen file

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sheake_it/main.dart';
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({
     Key? key,
@@ -14,7 +13,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  bool isInitialized = false;
+
   // late var   connectivityResult =  Connectivity().checkConnectivity();
 
   @override
@@ -22,209 +21,35 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     navigateToHome();
 
-    debugPrint("initState Of splashScreen is work now ! ");
-    // Timer(
-    //   const Duration(seconds: 3),
-    //   () => Navigator.pushReplacementNamed(context, MyPagesRoutes.main),
-    // );
-    // checkWifiStatus();
-
-//   Future<void> goToMain()async{
-//     connectivityResult = await (Connectivity().checkConnectivity());
-//   }
-    isInitialized = true ;
   }
 
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
 
-    // setState(() {
-    // });d
-
-    return   Scaffold(
+    return Scaffold(
         backgroundColor: const Color(0x2ef0f0fe),
-        body:
-        // isInitialized ?
-        // (
-        //   StreamBuilder<ConnectivityResult>(
-        //     stream: Connectivity().onConnectivityChanged,
-        //     builder: (context, snapshot) {
-        //      return snapshot.data == ConnectivityResult.wifi ||
-        //          snapshot.data == ConnectivityResult.mobile? const Main() : const NoInternet();
-        //
-        //   },
-        // )
-
-        // )
-        // :  bulidSplashScreenWidget()
-        SingleChildScrollView(
+        body: SingleChildScrollView(
             child: Center(
-              //
+                //
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset('images/assets/splash_screen.png')
- ]))));
-    //         // Text(
-    //         //   "وين",
-    //         //   style: TextStyle(
-    //         //     fontFamily: 'IBM',
-    //         //     fontSize: 70,
-    //         //     color: Colors.white,
-    //         //     // height: 4
-    //         //     // debugLabel: "Alamat"
-    //         //     // decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.lineThrough,TextDecoration.underline,TextDecoration.none])
-    //         //     //     fontStyle:FontStyle.normal
-    //         //     // inherit: true
-    //         //     // backgroundColor: Colors.grey
-    //         //     // fontWeight: FontWeight.w700
-    //         //     shadows: [
-    //         //       Shadow(
-    //         //           color: Colors.black38,
-    //         //           blurRadius: 20,
-    //         //           offset: Offset.fromDirection(2, 4))
-    //         //     ],
-    //         //   ),
-    //         //   textScaleFactor: 1.4,
-    //         //   // selectionColor: Colors.blue
-    //         // ),
-    //         // Text(
-    //         //   "بلاقي",
-    //         //   style: TextStyle(
-    //         //     // letterSpacing: 1,
-    //         //     fontFamily: 'IBM',
-    //         //     fontSize: 70,
-    //         //     color: Colors.white,
-    //         //     // height: 4
-    //         //     // debugLabel: "Alamat"
-    //         //     // decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.lineThrough,TextDecoration.underline,TextDecoration.none])
-    //         //     //     fontStyle:FontStyle.normal
-    //         //     // inherit: true
-    //         //     // backgroundColor: Colors.grey
-    //         //     // fontWeight: FontWeight.w700
-    //         //     shadows: [
-    //         //       Shadow(
-    //         //           color: Colors.black38,
-    //         //           blurRadius: 20,
-    //         //           offset: Offset.fromDirection(2, 4))
-    //         //     ],
-    //         //   ),
-    //         //   textScaleFactor: 1.4,
-    //         //   // selectionColor: Colors.blue
-    //         // ),
-    //       ],
-    //     ),
-    //   ),
-    // ),
-    // );
-
+              Image.asset('images/assets/splash_screen.png')
+            ]))));
   }
 
-
-  // StatelessWidget bulidSplashScreenWidget()  {
-
-  /*
-    this is first -v code
-     */
-  // Navigator.pushReplacementNamed(context, MyPagesRoutes.main);
-  // WidgetsBinding.instance.addPostFrameCallback((_) async {
-  //    isInitialized = false;
-  // });
-  //
-  //   return const SingleChildScrollView(
-  //     child: Center(
-  //       //
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           Image(
-  //             image: AssetImage("assets/images/splash_screen.jpg"),
-  //           )
-  //           // Text(
-  //           //   "وين",
-  //           //   style: TextStyle(
-  //           //     fontFamily: 'IBM',
-  //           //     fontSize: 70,
-  //           //     color: Colors.white,
-  //           //     // height: 4
-  //           //     // debugLabel: "Alamat"
-  //           //     // decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.lineThrough,TextDecoration.underline,TextDecoration.none])
-  //           //     //     fontStyle:FontStyle.normal
-  //           //     // inherit: true
-  //           //     // backgroundColor: Colors.grey
-  //           //     // fontWeight: FontWeight.w700
-  //           //     shadows: [
-  //           //       Shadow(
-  //           //           color: Colors.black38,
-  //           //           blurRadius: 20,
-  //           //           offset: Offset.fromDirection(2, 4))
-  //           //     ],
-  //           //   ),
-  //           //   textScaleFactor: 1.4,
-  //           //   // selectionColor: Colors.blue
-  //           // ),
-  //           // Text(
-  //           //   "بلاقي",
-  //           //   style: TextStyle(
-  //           //     // letterSpacing: 1,
-  //           //     fontFamily: 'IBM',
-  //           //     fontSize: 70,
-  //           //     color: Colors.white,
-  //           //     // height: 4
-  //           //     // debugLabel: "Alamat"
-  //           //     // decoration: TextDecoration.combine([TextDecoration.overline,TextDecoration.lineThrough,TextDecoration.underline,TextDecoration.none])
-  //           //     //     fontStyle:FontStyle.normal
-  //           //     // inherit: true
-  //           //     // backgroundColor: Colors.grey
-  //           //     // fontWeight: FontWeight.w700
-  //           //     shadows: [
-  //           //       Shadow(
-  //           //           color: Colors.black38,
-  //           //           blurRadius: 20,
-  //           //           offset: Offset.fromDirection(2, 4))
-  //           //     ],
-  //           //   ),
-  //           //   textScaleFactor: 1.4,
-  //           //   // selectionColor: Colors.blue
-  //           // ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-
-
-  // Get.to(() => const HomeView(),
-  //     transition: Transition.fade, duration: kTranstionDuratoin);
-
-
-  /*
-      this is 11/4/2023 -v
-       */
-  // await  Navigator.pushReplacementNamed(context, MyPagesRoutes.main);
-  // await  myPushName(context, MyPagesRoutes.main);
-
-  // Future.delayed(const Duration(seconds: 2), () {
-  //
-  //   // Get.to(() => const HomeView(),
-  //   //     transition: Transition.fade, duration: kTranstionDuratoin);
-  //
-  // });
-  // setState(() {
-  //   isInitialized = false;
-  // });
-  // }
-
   Future<void> navigateToHome() async {
-    Future.delayed(const Duration(milliseconds: 2000), () {
+    Future.delayed(const Duration(seconds: 2), () {
       // Navigator.pushReplacementNamed(context, MyPagesRoutes.noInternet);
 
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const NoInternet(),));
-
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const NoInternet(),
+          ));
 
       // checkWifiStatus();
     });
 
-
-    // Navigator.pushReplacementNamed(context,MyPagesRoutes.noInternet);
   }
 }
